c21c3dfab853d031824d1b71408ceacae9221003
free618 <freee618@126.com>
2016-11-15 19:44:30 +0800

    test version 81

---
+++ Android/update/81/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='81', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1813435, ['totalfilecount']=8, ['ziplist']={['1.zip']={['filesize']=1813435, ['filecount']=8,['md5']='5a2f024d87856755727db6185b6b72c7'}}
+local md5 = "a6d2a2bb25718e53fccce907984fcdc8";return info, md5
---
+++ IOS/update/81/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='81', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1878467, ['totalfilecount']=8, ['ziplist']={['1.zip']={['filesize']=1878467, ['filecount']=8,['md5']='a96c8c914532e57a8585e011a752bfdb'}}
+local md5 = "f87df9c3584e23dd76da95bfbb301218";return info, md5
