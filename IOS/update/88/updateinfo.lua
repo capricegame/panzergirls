c0275f4e0db20c47d78ab259af94b7f58a9bba35
free618 <freee618@126.com>
2016-11-15 20:58:20 +0800

    new version update data

---
+++ IOS/update/88/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='88', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1878467, ['totalfilecount']=329, ['ziplist']={['1.zip']={['filesize']=1878467, ['filecount']=329,['md5']='45a6c05e10b20338f8f4cfa5d55c0b00'}}}
+local md5 = "7513615e99beae1b834f98a973d4b99f";return info, md5
