20f5890c4b75e2b9b1351e2cf450fbcedcf2f228
free618 <freee618@126.com>
2016-11-15 22:42:20 +0800

    publish version 89

--- Android/update/89/updateinfo.lua
+++ Android/update/89/updateinfo.lua
@@ -1,2 +1,2 @@
-local info = {['version']='89', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1814900, ['totalfilecount']=329, ['ziplist']={['1.zip']={['filesize']=1814900, ['filecount']=329,['md5']='b79fc423859b1dbdd6194c235913b4ec'}}}
-local md5 = "7836e498ac93ad5b903a4ca14ce9f15f";return info, md5
+local info = {['version']='89', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1814903, ['totalfilecount']=329, ['ziplist']={['1.zip']={['filesize']=1814903, ['filecount']=329,['md5']='850d12891c2a4fdaf25e92caab5a975c'}}}
+local md5 = "36eebe1069527fd9e99d3ec92b2fb6ce";return info, md5
--- IOS/update/89/updateinfo.lua
+++ IOS/update/89/updateinfo.lua
@@ -1,2 +1,2 @@
-local info = {['version']='89', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1879204, ['totalfilecount']=329, ['ziplist']={['1.zip']={['filesize']=1879204, ['filecount']=329,['md5']='8b6a23f1b10d44ef4bd8d0079cc4fbde'}}}
-local md5 = "65daa698078b13344ea349eca89729e4";return info, md5
+local info = {['version']='89', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1879207, ['totalfilecount']=329, ['ziplist']={['1.zip']={['filesize']=1879207, ['filecount']=329,['md5']='e0b9076998372430eee89cb08642444f'}}}
+local md5 = "d1c2e79ee083348434f82b045f0309d7";return info, md5
