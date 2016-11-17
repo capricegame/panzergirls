015e4ee914904f5ef5093bd6507f8a655f86b47d
free618 <freee618@126.com>
2016-11-18 00:51:20 +0800

    version 92

---
+++ Android/update/92/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='92', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1815078, ['totalfilecount']=329, ['ziplist']={['1.zip']={['filesize']=1815078, ['filecount']=329,['md5']='f8e3c497a4657ef42e3d9ded945d4fb2'}}}
+local md5 = "87330cc2c514b7cb65c285846ef42488";return info, md5
---
+++ IOS/update/92/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='92', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1879397, ['totalfilecount']=329, ['ziplist']={['1.zip']={['filesize']=1879397, ['filecount']=329,['md5']='27f65d090a3918a680e0f29a29f94ba7'}}}
+local md5 = "20dc0173ab5af98348608cc9015d5dea";return info, md5
