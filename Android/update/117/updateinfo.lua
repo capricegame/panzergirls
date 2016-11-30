1961d4946497f59ddb473764d11767112798a687
free618 <freee618@126.com>
2016-11-30 20:52:46 +0800

    version 96 to 117

---
+++ Android/update/117/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='117', ['zipcount']=1, ['deletelist']={}, ['totalsize']=2903923, ['totalfilecount']=324, ['ziplist']={['1.zip']={['filesize']=2903923, ['filecount']=324,['md5']='802a3f77dc810a574797a0e9ac4ab310'}}}
+local md5 = "a912cb9acaef5a78db32e160f27d735e";return info, md5
---
+++ IOS/update/117/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='117', ['zipcount']=1, ['deletelist']={}, ['totalsize']=2968294, ['totalfilecount']=324, ['ziplist']={['1.zip']={['filesize']=2968294, ['filecount']=324,['md5']='814dd4ef1fc36bfe648df04abea01529'}}}
+local md5 = "4a0f75e8d37b869045a7a06c12196777";return info, md5
