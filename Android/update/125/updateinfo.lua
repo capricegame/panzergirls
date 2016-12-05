5dc03e3ab09f5464aefba8aab4d653b9398d4764
free618 <freee618@126.com>
2016-12-05 14:29:35 +0800

    117 to 125 update patch

---
+++ Android/update/125/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='125', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1509339, ['totalfilecount']=322, ['ziplist']={['1.zip']={['filesize']=1509339, ['filecount']=322,['md5']='074a4648c22d2aee58f0457ad6153c74'}}}
+local md5 = "37ba4afbe0afddb9cf8ffe76a10e3c76";return info, md5
---
+++ IOS/update/125/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='125', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1573721, ['totalfilecount']=322, ['ziplist']={['1.zip']={['filesize']=1573721, ['filecount']=322,['md5']='d6cc10c1034281487c6e7a1d6ce673aa'}}}
+local md5 = "63f4d151fb30345b7b17c4c620713672";return info, md5
