e25c7d2581568c5d408e86a1feb7da0dfb04ffc4
free618 <freee618@126.com>
2016-12-05 19:27:15 +0800

    125 to 127

---
+++ Android/update/127/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='127', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1474751, ['totalfilecount']=323, ['ziplist']={['1.zip']={['filesize']=1474751, ['filecount']=323,['md5']='c409fbc366d4294d33f1e26a7dec5c5c'}}}
+local md5 = "652feb175e901715b57f86d513f63ed7";return info, md5
---
+++ IOS/update/127/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='127', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1539133, ['totalfilecount']=323, ['ziplist']={['1.zip']={['filesize']=1539133, ['filecount']=323,['md5']='dddbf906e9d84cf6af13032a1cb54177'}}}
+local md5 = "8edd985664944863d7ef832f6d666f3c";return info, md5
