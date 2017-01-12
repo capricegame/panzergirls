9719490515d2ec407c9cec26f09f2bfc462fece1
dkpyihong <dkpyihong@gmal.com>
2017-01-13 03:48:46 +0800

    153 to 318 version

---
+++ IOS/update/318/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='318', ['zipcount']=1, ['deletelist']={}, ['totalsize']=19610023, ['totalfilecount']=913, ['ziplist']={['1.zip']={['filesize']=19610023, ['filecount']=913,['md5']='a1d037a69f312de6f2f53963a2dd73a6'}}}
+local md5 = "ab07e978f54950da48e99d016dffdc44";return info, md5
