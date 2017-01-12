ddfc81e1fb0d096f2a4fb3178d3353fc55691a68
dkpyihong <dkpyihong@gmal.com>
2017-01-13 03:54:50 +0800

    153 to 318 version

---
+++ Android/update/318/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='318', ['zipcount']=1, ['deletelist']={}, ['totalsize']=18014682, ['totalfilecount']=592, ['ziplist']={['1.zip']={['filesize']=18014682, ['filecount']=592,['md5']='6a81b6014e3dad411fa76c2e4e29b9f1'}}}
+local md5 = "ef7b78a27d7e41a53985e80e171cbb78";return info, md5
