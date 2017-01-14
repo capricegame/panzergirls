fe45eeda9eaf582d68807d6abae0f70d07b7ea35
dkpyihong <dkpyihong@gmal.com>
2017-01-14 19:29:35 +0800

    319 -> 322.

---
+++ Android/update/322/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='322', ['zipcount']=1, ['deletelist']={}, ['totalsize']=4632391, ['totalfilecount']=345, ['ziplist']={['1.zip']={['filesize']=4632391, ['filecount']=345,['md5']='88fc268b4526b96c97888bdbf6f20326'}}}
+local md5 = "cd6886fe930a1aa29f934eaaabf4334d";return info, md5
---
+++ IOS/update/322/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='322', ['zipcount']=1, ['deletelist']={}, ['totalsize']=6230464, ['totalfilecount']=666, ['ziplist']={['1.zip']={['filesize']=6230464, ['filecount']=666,['md5']='b42cef8cec9e1f29b2ed1c0e3b720492'}}}
+local md5 = "455c4396eb2cd1cf0ab40c48e28d7efe";return info, md5
