23255d7bdb16789a16458b660b2d5c94eedec49c
dkpyihong <dkpyihong@gmail.com>
2017-03-26 01:29:01 +0800

    update 477-482

---
+++ Android/update/482/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='482', ['zipcount']=1, ['deletelist']={}, ['totalsize']=1534777, ['totalfilecount']=326, ['ziplist']={['1.zip']={['filesize']=1534777, ['filecount']=326,['md5']='19f625a1036bbd07860ecf390ca88d1f'}}}
+local md5 = "040b9865a3b9c7432264764c00cd54ac";return info, md5
---
+++ IOS/update/482/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='482', ['zipcount']=1, ['deletelist']={}, ['totalsize']=3152042, ['totalfilecount']=650, ['ziplist']={['1.zip']={['filesize']=3152042, ['filecount']=650,['md5']='eabdeb8930925a526e4b6af950724fce'}}}
+local md5 = "13a2db669d716d4141dfff572ae84304";return info, md5
