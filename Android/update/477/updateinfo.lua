b051e3b2ee562a1a8fe1ebcae450ed30e82da96e
dkpyihong <dkpyihong@gmail.com>
2017-03-25 23:21:10 +0800

    add udpate 411-477

---
+++ Android/update/477/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='477', ['zipcount']=1, ['deletelist']={}, ['totalsize']=4170969, ['totalfilecount']=354, ['ziplist']={['1.zip']={['filesize']=4170969, ['filecount']=354,['md5']='95317e0b992f15059854e0910751db65'}}}
+local md5 = "5683ca1cb9c13e8b74ed0db293f41462";return info, md5
---
+++ IOS/update/477/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='477', ['zipcount']=1, ['deletelist']={}, ['totalsize']=5788230, ['totalfilecount']=678, ['ziplist']={['1.zip']={['filesize']=5788230, ['filecount']=678,['md5']='bf6ec202a73deb552338fb56cae4ad5b'}}}
+local md5 = "aad1c66d98368affe410da630aeec653";return info, md5
