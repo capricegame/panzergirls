03906ca400cd4271d421816683aca29a58c12055
dkpyihong <dkpyihong@gmail.com>
2017-05-05 21:56:37 +0800

    update for 788

---
+++ Android/update/788/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='788', ['zipcount']=1, ['deletelist']={}, ['totalsize']=11309588, ['totalfilecount']=415, ['ziplist']={['1.zip']={['filesize']=11309588, ['filecount']=415,['md5']='573adfdf82bbfea3fd30338616e5019e'}}}
+local md5 = "dbff3524f01e958ccfcfafa7e09ce4e0";return info, md5
---
+++ IOS/update/788/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='788', ['zipcount']=1, ['deletelist']={}, ['totalsize']=12945304, ['totalfilecount']=740, ['ziplist']={['1.zip']={['filesize']=12945304, ['filecount']=740,['md5']='4eebc29a033108783db8ece6733d2107'}}}
+local md5 = "31f72aef73fbd947beff4ac7dab6f881";return info, md5
