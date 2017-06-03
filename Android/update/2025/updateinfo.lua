d9cb0e67ba5c00fa8fb716d6bed71ea7c9f469f6
dkpyihong <dkpyihong@gmail.com>
2017-06-03 23:36:06 +0800

    upadte version 2025

---
+++ Android/update/2025/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='2025', ['zipcount']=1, ['deletelist']={}, ['totalsize']=2982757, ['totalfilecount']=414, ['ziplist']={['1.zip']={['filesize']=2982757, ['filecount']=414,['md5']='a2da21f5d97012c4bba5c49211408015'}}}
+local md5 = "43536406230a6323647e00daa19653ca";return info, md5
---
+++ IOS/update/2025/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='2025', ['zipcount']=1, ['deletelist']={}, ['totalsize']=4950712, ['totalfilecount']=762, ['ziplist']={['1.zip']={['filesize']=4950712, ['filecount']=762,['md5']='466a43e4621c73502361f08a22fb3236'}}}
+local md5 = "91ac24b8f6fbf8ac3aa0af4063dcd272";return info, md5
