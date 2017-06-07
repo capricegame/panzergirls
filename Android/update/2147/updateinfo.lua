bcf67f2fe0076147b8df7cc465964992ba86d00d
dkpyihong <dkpyihong@gmail.com>
2017-06-08 01:17:16 +0800

    update version 2147

---
+++ Android/update/2147/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='2147', ['zipcount']=1, ['deletelist']={}, ['totalsize']=19494516, ['totalfilecount']=874, ['ziplist']={['1.zip']={['filesize']=19494516, ['filecount']=874,['md5']='36e181446ad944617d543c4a9f5194e4'}}}
+local md5 = "b73b46c6687103d880c6718a4c9e8bc8";return info, md5
---
+++ IOS/update/2147/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='2147', ['zipcount']=1, ['deletelist']={}, ['totalsize']=21639408, ['totalfilecount']=1241, ['ziplist']={['1.zip']={['filesize']=21639408, ['filecount']=1241,['md5']='98266aed942f027563374d0ab0970afe'}}}
+local md5 = "ab7d3b30197d76ad66790187d0a9ec71";return info, md5
