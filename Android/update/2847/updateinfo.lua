53281405a9ee0ba56cae2544fa1f91b724b48b2b
dkpyihong <dkpyihong@gmail.com>
2017-07-15 01:55:36 +0800

    commit 3101

---
+++ Android/update/2847/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='2847', ['zipcount']=1, ['deletelist']={}, ['totalsize']=10660427, ['totalfilecount']=454, ['ziplist']={['1.zip']={['filesize']=10660427, ['filecount']=454,['md5']='ecf23021c5e4c018d4484bf700c4f1f8'}}}
+local md5 = "4bd42cb77053c10de37b47c97c59faa4";return info, md5
---
+++ IOS/update/2847/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='2847', ['zipcount']=1, ['deletelist']={}, ['totalsize']=12901593, ['totalfilecount']=806, ['ziplist']={['1.zip']={['filesize']=12901593, ['filecount']=806,['md5']='3a06cbc1c9c12e439e2957fb8b32573c'}}}
+local md5 = "d9638ba6e452c77b06b393e45dfa63d7";return info, md5
