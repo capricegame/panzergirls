dd16768aa76f96dadb3b7278dd18c2366599b2fe
dkpyihong <dkpyihong@gmail.com>
2018-07-03 14:47:27 +0800

    update version 3445-3459

---
+++ Android/update/3459/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3459', ['zipcount']=1, ['deletelist']={}, ['totalsize']=2593584, ['totalfilecount']=364, ['ziplist']={['1.zip']={['filesize']=2593584, ['filecount']=364,['md5']='632d4fd2652f71907c7501de4a3ffbd4'}}}
+local md5 = "5042e6f85f1aae57f21ff257572ee2fd";return info, md5
---
+++ IOS/update/3459/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3459', ['zipcount']=1, ['deletelist']={}, ['totalsize']=5108409, ['totalfilecount']=728, ['ziplist']={['1.zip']={['filesize']=5108409, ['filecount']=728,['md5']='17da59402da801ac3c594a626fa7a496'}}}
+local md5 = "f74638c87991120bbace6bbd93279493";return info, md5
