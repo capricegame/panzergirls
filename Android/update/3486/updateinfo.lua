6d3c249bdf9f10d84475805130014c03e1e1e008
dkpyihong <dkpyihong@gmail.com>
2018-11-28 16:42:00 +0800

    udpate version 3477 - 3486

---
+++ Android/update/3486/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3486', ['zipcount']=1, ['deletelist']={"res/images/cards/VK2001/DamagedImage.png","res/images/cards/VK2001/NormalImage.png"}, ['totalsize']=5988054, ['totalfilecount']=393, ['ziplist']={['1.zip']={['filesize']=5988054, ['filecount']=393,['md5']='157894a363b2b750563d3796a68fb8cc'}}}
+local md5 = "73cbdb1c25fb75fd5a0594df6ce198ff";return info, md5
---
+++ IOS/update/3486/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3486', ['zipcount']=1, ['deletelist']={"res/images/cards/VK2001/DamagedImage.png","res/images/cards/VK2001/NormalImage.png"}, ['totalsize']=9774848, ['totalfilecount']=757, ['ziplist']={['1.zip']={['filesize']=9774848, ['filecount']=757,['md5']='5c2bbdeb015a6e46039dbf80344bdb19'}}}
+local md5 = "c903110d408a4eceeff36ccdf3b1b527";return info, md5
