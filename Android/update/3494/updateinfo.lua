a12f442719ff43fc60e23e95b90f4224c6cc3f6a
dkpyihong <dkpyihong@gmail.com>
2019-01-03 17:04:04 +0800

    update version 3486-3494

---
+++ Android/update/3494/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3494', ['zipcount']=1, ['deletelist']={}, ['totalsize']=2628701, ['totalfilecount']=364, ['ziplist']={['1.zip']={['filesize']=2628701, ['filecount']=364,['md5']='1d9ffe4d349210cf7208c20d93dbeae9'}}}
+local md5 = "12f0747d02784ffc051b1d4090f5bafa";return info, md5
---
+++ IOS/update/3494/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3494', ['zipcount']=1, ['deletelist']={}, ['totalsize']=5167790, ['totalfilecount']=728, ['ziplist']={['1.zip']={['filesize']=5167790, ['filecount']=728,['md5']='2fdaf0b66b6d586bd5ceda308f9d1f49'}}}
+local md5 = "88da20a6c825bb5c127d2c5339269565";return info, md5
