fc77d41ec6b67420f8897bed59893d0c6e4a71c2
dkpyihong <dkpyihong@gmail.com>
2019-01-04 15:19:03 +0800

    udpate version 3494 - 3496

---
+++ Android/update/3496/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3496', ['zipcount']=1, ['deletelist']={}, ['totalsize']=2628692, ['totalfilecount']=364, ['ziplist']={['1.zip']={['filesize']=2628692, ['filecount']=364,['md5']='43c0b7e36525c0131e20924446442947'}}}
+local md5 = "a060b5eb537f8a2357823dfb2875edd2";return info, md5
---
+++ IOS/update/3496/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3496', ['zipcount']=1, ['deletelist']={}, ['totalsize']=5167767, ['totalfilecount']=728, ['ziplist']={['1.zip']={['filesize']=5167767, ['filecount']=728,['md5']='293d585712bce2db741a48f662220311'}}}
+local md5 = "9a2a40beb2f43216f0c64681a14f78cc";return info, md5
