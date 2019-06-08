118158392fcabe39a2c4b7f7f7a0db71c6404ee0
dkpyihong <dkpyihong@gmail.com>
2019-06-08 12:51:32 +0800

    update 3505-3509

---
+++ Android/update/3509/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3509', ['zipcount']=1, ['deletelist']={}, ['totalsize']=2642651, ['totalfilecount']=365, ['ziplist']={['1.zip']={['filesize']=2642651, ['filecount']=365,['md5']='8c09d060e707d3e281b5ee59d3cf882b'}}}
+local md5 = "a333ac03e98043779d7a75f84197dee0";return info, md5
---
+++ IOS/update/3509/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3509', ['zipcount']=1, ['deletelist']={}, ['totalsize']=5189825, ['totalfilecount']=730, ['ziplist']={['1.zip']={['filesize']=5189825, ['filecount']=730,['md5']='a7b4871f8fc9d577e2b6bf5c74c9e269'}}}
+local md5 = "a4ed76f2430f22be9f3e860a9981c4e3";return info, md5
