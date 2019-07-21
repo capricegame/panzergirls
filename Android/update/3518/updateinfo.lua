48e0af651120e908691b0dee9a9846d018745e04
dkpyihong <dkpyihong@gmail.com>
2019-07-21 19:22:18 +0800

    udpate version 3509-3518

---
+++ Android/update/3518/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3518', ['zipcount']=1, ['deletelist']={}, ['totalsize']=2676813, ['totalfilecount']=365, ['ziplist']={['1.zip']={['filesize']=2676813, ['filecount']=365,['md5']='3993af244daaf3210961a86e689c5740'}}}
+local md5 = "7092cea0ebce7ebcb393cef79462df2b";return info, md5
---
+++ IOS/update/3518/updateinfo.lua
@@ -0,0 +1,2 @@
+local info = {['version']='3518', ['zipcount']=1, ['deletelist']={}, ['totalsize']=5245578, ['totalfilecount']=730, ['ziplist']={['1.zip']={['filesize']=5245578, ['filecount']=730,['md5']='95bdab97c8bc293c75e9baffedcfce99'}}}
+local md5 = "eab9ec90bb9260b3c6e5fcf8c9c1d5e0";return info, md5
